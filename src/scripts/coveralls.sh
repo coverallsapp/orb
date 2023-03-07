91981a35b8d180233fb9166be9426ac4206955a1
Valentin Kiselev <mrexox@evilmartians.com>
2023-03-07 10:11:33 +0300

    fix: add verbose steps

--- src/scripts/coveralls.sh
+++ src/scripts/coveralls.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 if [ ! "${COVERALLS_REPO_TOKEN}" ]; then
-  COVERALLS_REPO_TOKEN=$(printenv "${COVERALLS_REPO_TOKEN_ENV}")
+  COVERALLS_REPO_TOKEN=$(printenv "${COVERALLS_REPO_TOKEN_ENV}") || (echo "Token not configured" && exit 1)
   export COVERALLS_REPO_TOKEN
 fi
 
-if "${COVERALLS_DONE}"; then
+if [ "${COVERALLS_DONE}" == "1" ]; then
+  echo "Sending parallel finish webhook"
+
+  [ "${COVERALLS_DRY_RUN}" == "1" ] && exit 0
+
   curl "${COVERALLS_ENDPOINT}/webhook?repo_token=${COVERALLS_REPO_TOKEN}&carryforward=${COVERALLS_CARRYFORWARD_FLAGS}" \
     -d "payload[build_num]=${CIRCLE_WORKFLOW_ID}&payload[status]=done"
 
@@ -26,7 +30,11 @@ if [ ! -r "${COVERALLS_COVERAGE_FILE}" ]; then
   exit 1
 fi
 
-if "${COVERALLS_VERBOSE}"; then
+echo "Processing coverage from ${COVERALLS_COVERAGE_FILE}"
+
+[ "${COVERALLS_DRY_RUN}" == "1" ] && exit 0
+
+if [ "${COVERALLS_VERBOSE}" == "1" ]; then
   coveralls --verbose < "${COVERALLS_COVERAGE_FILE}"
 else
   coveralls < "${COVERALLS_COVERAGE_FILE}"
